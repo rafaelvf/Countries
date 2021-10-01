783dc8902f1e2cdfd4cf9667ddfa09d298121338
Rafael Viteri <reviterif@gmail.com>
2021-10-01 12:29:11 -0500

    final 2

--- api/src/controllers/activities.js
+++ api/src/controllers/activities.js
@@ -1,49 +1,48 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const axios = require("axios");
-const {Sequelize}=require("sequelize")
+const { Sequelize } = require("sequelize");
 
+const { Country, Activity } = require("../db.js"); //deberia funcionar
 
-const {Country, Activity} = require("../db.js");//deberia funcionar
+router.post("/", async function (req, res) {
+  const { ID, name, dificulty, duration, season, countries } = req.body; //por body me va a llegar estas porpiedades. que vienen del form
 
-router.post("/", async function(req,res){
-    const {ID, name, dificulty, duration, season,countries } = req.body;//por body me va a llegar estas porpiedades. que vienen del form
-    console.log(req.body)
-
-    const [nuevaActividad,created]= await Activity.findOrCreate({
-        where:{
-            // ID: ID,
-            name: name,
-            dificulty:dificulty,
-            duration:duration,
-            season:season
-        }
-    })
-
-    console.log(nuevaActividad)
-    for(const i of countries){
-    const country= await Country.findOne({
-        where:{
-            name:i
-        }
-    })
-    // nuevaActividad.addCountry(country)
-    country.addActivity(nuevaActividad)
-}
+    try {
     
+        const [nuevaActividad, created] = await Activity.findOrCreate({
+            where: {
+              // ID: ID,
+                name: name,
+                dificulty: dificulty,
+                duration: duration,
+                season: season,
+            },
+        });
+        
+        for (const i of countries) {
+            const country = await Country.findOne({
+                where: {
+                    name: i,
+                },
+            });
+            
+            country.addActivity(nuevaActividad);
+        }
+        
+        res.json(nuevaActividad);
+        
+    } catch (error) {
+        console.error(error)
+    }
+});
 
-    res.json(nuevaActividad);
+router.get("/add", async function (req, res) {
+  const justactivities = await Activity.findAll({
+    attributes: ["name"],
+  });
 
+  res.json(justactivities);
 
-})
+});
 
-router.get("/add", async function(req,res){
-
-    const justactivities= await Activity.findAll({
-        attributes:["name"]
-    })
-
-        res.json(justactivities)
-})
-
-
-module.exports=router;
+module.exports = router;
--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -1,94 +1,93 @@
-import React,{useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Card from "./Card";
-import {connect,useSelector} from "react-redux";
-import { getAll } from '../actions/actions';
-import Paginado from './Paginado';
-import "../css/CardContainer.css"
+import { connect, useSelector } from "react-redux";
+import { getAll } from "../actions/actions";
+import Paginado from "./Paginado";
+import "../css/CardContainer.css";
 
+export function CardContainer(props) {
+  //props = {country:state.country[],getcountries:function}
+  useEffect(() => {
+    props.getcountries();
+  }, [props]); //con useEffect estamos cargando el estado porque set esta ejecunta elgetcountries que tiene adentro el dispatch la action getall
 
-export function CardContainer(props) {//props = {country:state.country[],getcountries:function}
-  useEffect(()=>{props.getcountries()},[props])//con useEffect estamos cargando el estado porque set esta ejecunta elgetcountries que tiene adentro el dispatch la action getall
-  
-  const {countries}=useSelector((state)=>state.allCountries)
+  const { countries } = useSelector((state) => state.allCountries);
 
-  const [currentPage,setCurrentPage]=useState(1);
-  const [postsPerPage]=useState(9);
+  const [currentPage, setCurrentPage] = useState(1); //pagina actual
+  const [postsPerPage] = useState(9); //cuantas cartas desplegamos por pagina
 
-  const [pageNumberLimit]=useState(9);//cuantas numeros de pagina queremos display
-  const [maxPageNumberLimit,setmaxPageNumberLimit]=useState(9);
-  const [minPageNumberLimit,setminPageNumberLimit]=useState(0);  
+  const [pageNumberLimit] = useState(9); //limite de numeros que queremos ver en el paginado
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(9); //maximo de numeros que queremos ver en el paginado
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0); //minimo de numeros que queremos vern el paginado
 
+  const indexOfLastPost = currentPage * postsPerPage; //pagina actual * numero de cartas por pagina--9*5=45
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; //index de laultiam pagina - cuantos post per page hay-[45-9]
+  const currentPosts = countries.slice(indexOfFirstPost, indexOfLastPost); //aqui hacemos la division slice para tener los posts por pagina.
 
-  const indexOfLastPost= currentPage*postsPerPage;
-  const indexOfFirstPost=indexOfLastPost-postsPerPage;
-  const currentPosts=countries.slice(indexOfFirstPost,indexOfLastPost)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber); //esta es una funcion que recibe un numero y cambia el estado de current page a ese numero.
 
-  const paginate =(pageNumber)=>setCurrentPage(pageNumber)
-  console.log("conosloeee log",paginate)
-  const handleNextbtn=()=>{
-    setCurrentPage(currentPage+1);
+  const handleNextbtn = () => {
+    //esta es una funcion que cambia el estado de current page +1 para avanazar una pagina.
+    setCurrentPage(currentPage + 1);
 
-    if(currentPage+1>maxPageNumberLimit){
-      setmaxPageNumberLimit(maxPageNumberLimit+pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit+pageNumberLimit);
+    if (currentPage + 1 > maxPageNumberLimit) {//para cambiar los numero que enseña. se despalza
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
-  }
+  };
 
-  const handlePrevbtn = ()=>{
-    setCurrentPage(currentPage-1);
+  const handlePrevbtn = () => {
+    //funcion pra el boton de retroceder
+    setCurrentPage(currentPage - 1);
 
-    if((currentPage-1)% pageNumberLimit===0){
-      setmaxPageNumberLimit(maxPageNumberLimit-pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit-pageNumberLimit);
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
     }
-  }
+  };
 
-  console.log("current post"+ currentPosts)//9 cartas
+  console.log("current post" + currentPosts); //9 cartas
 
-  return currentPosts && currentPosts.length ?(
+  return currentPosts && currentPosts.length ? (
     <div>
-    {/* {
-    <Paginado postsPerPage={postsPerPage} totalPosts={countries.length} paginate={paginate} minPageNumberLimit={minPageNumberLimit}  maxPageNumberLimit={maxPageNumberLimit}
-    handleNextbtn={handleNextbtn} handlePrevbtn={handlePrevbtn}
-    currentPage={currentPage}/>
-    } */}
-
-    <div className="cardcontainer">
-    
-  { currentPosts.map(i=>{
-    console.log("f"+currentPosts)
-    
-      return (
-        <div key={i.ID} className="j"> {/* para que cada card tenga una id en el navegador, y no moeleste la consola del navegador */}
-          <Link to={`/countries/${i.ID}`}>
-          <Card country={i.name} flag={i.flag} continent={i.continent} />
-          </Link>
-        </div> 
-      )
       
-    })}
-    
+
+      <div className="cardcontainer">
+        {currentPosts.map((i) => {
+          console.log("f" + currentPosts);
+
+          return (
+            <div key={i.ID} className="j">
+              {" "}
+              {/* para que cada card tenga una id en el navegador, y no moeleste la consola del navegador */}
+              <Link to={`/countries/${i.ID}`}>
+                <Card country={i.name} flag={i.flag} continent={i.continent} />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      {
+        <Paginado
+          postsPerPage={postsPerPage}
+          totalPosts={countries.length}
+          paginate={paginate}
+          minPageNumberLimit={minPageNumberLimit}
+          maxPageNumberLimit={maxPageNumberLimit}
+          handleNextbtn={handleNextbtn}
+          handlePrevbtn={handlePrevbtn}
+          currentPage={currentPage}
+        />
+      }
     </div>
-    {
-    <Paginado postsPerPage={postsPerPage} totalPosts={countries.length} paginate={paginate} minPageNumberLimit={minPageNumberLimit}  maxPageNumberLimit={maxPageNumberLimit}
-    handleNextbtn={handleNextbtn} handlePrevbtn={handlePrevbtn}
-    currentPage={currentPage}/>
-    }
-    </div>
-  ):<p>The country does not exist!</p>
-};
-
-
-// function mapStateToProps(state){
-//   return{
-//       country:state.allCountries.countries,//country se guarda lo que necesito del state del store
-      
-//   }
-// }
-function mapDispatchToProps(dispatch){
-
-  return{getcountries:()=>dispatch(getAll())}
+  ) : (
+    <p>The country does not exist!</p>
+  );
 }
 
-export default connect(null,mapDispatchToProps)(CardContainer);
+function mapDispatchToProps(dispatch) {
+  return { getcountries: () => dispatch(getAll()) };
+}
+
+export default connect(null, mapDispatchToProps)(CardContainer);
